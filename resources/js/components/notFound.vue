45318c81827747c96f982f41b8f271ae2807955a
Josh <villanuevajosh212@gmail.com>
2025-01-09 12:02:22 +0800

    Fixed Assets

--- resources/js/components/notFound.vue
+++ resources/js/components/notFound.vue
@@ -1,14 +1,13 @@
-<script setup>
-import pageNotFoundVector from "/Projects/Development/crud-app/public/assets/page-not-found-vector.svg";
-</script>
-
 <template>
     <div class="container">
         <div
             class="row d-flex flex-column align-items-center justify-content-center min-vh-100"
         >
             <div class="col-md-6">
-                <img :src="pageNotFoundVector" alt="" />
+                <img
+                    src="/assets/page-not-found-vector.svg"
+                    alt="Page Not Found"
+                />
                 <p class="fw-bold fs-4 text-center">
                     Page Not Found.
                     <router-link to="/">Back to Home?</router-link>
